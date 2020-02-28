c7da97459f1e31dec94e7c5e348b1b2a83edc446
Ray H <rjhern37@neiu.edu>
2020-02-28 09:05:41 -0600

    adding a question page

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+var countdown = function(num) {
+    for (var i = num; i > 0; i--) {
+      console.log(i);
+    }
+  };
+  */
+
+  $('button').on('click', function(){
+      alert( 'You clicked the BUTTON')
+  })
